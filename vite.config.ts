b23840d0d5a4028e4c9ce5c924b93c6fb7950f68
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2026-04-12 14:45:25 -0300

    fix: update base path in Vite configuration for gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/',
+    base: '/lista-compras-gh-pages/',
     build: {
         outDir: 'build',
         minify: 'terser',
